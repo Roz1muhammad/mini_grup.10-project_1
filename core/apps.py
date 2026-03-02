c7709b8c9d00f1b046e444bde3581884bc021b0b
Rozivoy <dehqonaliyevrozivoy33@gmail.com>
2026-03-02 11:27:45 +0500

    update

--- core/apps.py
+++ core/apps.py
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
+
 from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
     name = 'core'
-=======
-from django.apps import AppConfig
 
 
-class CoreConfig(AppConfig):
-    name = 'core'
->>>>>>> 439e80ce14ba54fdabe22a03f67c45cee528e572
